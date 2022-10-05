d341fa4ddac4c7b1560542ffb7cbb517b9a89c29
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-10-05 05:47:38 +0300

    Update and rename hello.lua to attenu.R

---
+++ attenu.R
@@ -0,0 +1,9 @@
+attenu
+require(graphics)
+## check the data class of the variables
+sapply(attenu, data.class)
+summary(attenu)
+pairs(attenu, main = "attenu data")
+coplot(accel ~ dist | as.factor(event), data = attenu, show.given = FALSE)
+coplot(log(accel) ~ log(dist) | as.factor(event),
+       data = attenu, panel = panel.smooth, show.given = FALSE)
--- hello.lua
+++
@@ -1,4 +0,0 @@
--- hello.lua
--- the first program in every language
-
-io.write("E = mcmod2, from ",_VERSION,"!\n")
